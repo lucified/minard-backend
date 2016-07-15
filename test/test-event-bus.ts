356a4dd8800d496fca41d9eb5b587875f7d69b99
Juho Ojala <juho@lucify.com>
2016-07-15 11:09:10 +0300

    Add simple test for EventBus

---
+++ test/test-event-bus.ts
@@ -0,0 +1,21 @@
+
+import EventBus from '../src/event-bus';
+import { expect } from 'chai';
+
+describe('event-bus', () => {
+
+  it('should work with single event', done => {
+    const bus = new EventBus();
+    bus
+      .subscribe((event : any) => {
+        expect(event.type).to.equal('test-event');
+        done();
+      });
+    const event = {
+      type: 'test-event'
+    }
+    bus.post(event);
+  });
+
+});
+

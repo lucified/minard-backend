136bc62d05e62043f388559dab314a2b25de058a
Juho Ojala <juho@lucify.com>
2016-07-18 19:49:03 +0300

    Edit inversify example to symbols from abstract class

--- src/scripts/inversify-test.ts
+++ src/scripts/inversify-test.ts
@@ -2,20 +2,30 @@
 /// <reference path="../../node_modules/inversify-dts/inversify/inversify.d.ts" />
 import { injectable, inject, Kernel } from 'inversify';
 import "reflect-metadata";
+//import { Jepa, name } from '../Jepa';
 
 let TYPES = {
-  Jepa: Symbol('jepa'),
-  Hepo: Symbol('hepa')
+  Jepa: Symbol(),
+  Hepo: Symbol()
 }
 
-interface Jepa {
-  hello(): void;
+abstract class Jepa {
+  static injectName = Symbol();
+  abstract hello();
+}
+
+abstract class Bepa {
+  static injectName = Symbol();
+  abstract whoa();
 }
 
 @injectable()
-class JepaA implements Jepa {
+class JepaA implements Jepa, Bepa {
   hello() {
     console.log('JepaA');
+  }
+  whoa() {
+    console.log('whoaA');
   }
 }
 
@@ -29,18 +39,19 @@ class JepaB implements Jepa {
 @injectable()
 class Hepo {
   jepa: Jepa;
-  constructor(@inject(TYPES.Jepa) jepa : Jepa) {
+  constructor(@inject(Jepa.injectName) jepa : Jepa) {
     this.jepa = jepa;
   }
 }
 
-var kernel1 = new Kernel();
-kernel1.bind<Jepa>(TYPES.Jepa).to(JepaA);
-var kernel2 = new Kernel();
-kernel2.bind<Jepa>(TYPES.Jepa).to(JepaB);
 
-kernel1.bind<Hepo>(TYPES.Hepo).to(Hepo);
-kernel2.bind<Hepo>(TYPES.Hepo).to(Hepo);
+var kernel1 = new Kernel();
+kernel1.bind(Jepa.injectName).to(JepaA);
+var kernel2 = new Kernel();
+kernel2.bind(Jepa.injectName).to(JepaB);
+
+kernel1.bind(TYPES.Hepo).to(Hepo);
+kernel2.bind(TYPES.Hepo).to(Hepo);
 
 const hepo1 = kernel1.get<Hepo>(TYPES.Hepo);
 hepo1.jepa.hello();

2b0d490d9a11774cb509ab9da1d7110f3f4e68be
Juho Ojala <juho@lucify.com>
2016-07-21 17:33:08 +0300

    Add functions for converting gitlab builds responses to json api

---
+++ src/deployment/deployment-module.ts
@@ -0,0 +1,44 @@
+
+const Serializer = require('jsonapi-serializer').Serializer; // tslint:disable-line
+
+
+export function gitlabResponseToJsonApi(gitlabResponse: any) {
+  const normalized = normalizeGitLabResponse(gitlabResponse);
+  const opts = {
+    attributes: ['finished_at', 'status', 'commit', 'user'],
+    commit: {
+       attributes: ['message'],
+       ref: function (_: any, commit: any) {
+          return String(commit.id);
+       },
+    },
+    user: {
+       attributes: ['username'],
+       ref: function (_: any, user: any) {
+          return String(user.id);
+       },
+    },
+  };
+  const serialized = new Serializer('deployment', opts).serialize(normalized);
+  console.log(JSON.stringify(serialized, null, 3));
+  return serialized;
+};
+
+export function normalizeGitLabResponse(gitlabResponse: any) {
+  return gitlabResponse.map((item: any) => {
+    return {
+      id: item.id,
+      user: {
+        id: item.user.id,
+        username: item.user.username,
+      },
+      commit: {
+        id: item.commit.id,
+        message: item.commit.message,
+      },
+      finished_at: item.finished_at,
+      status: item.status,
+    };
+  });
+};
+

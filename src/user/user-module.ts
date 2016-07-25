3699e9fa8715d22af0ac943a6e5e269302682d60
Juho Ojala <juho@lucify.com>
2016-07-25 15:49:38 +0300

    Use injection for UserModule and port related unit test to use mock db

--- src/user/user-module.ts
+++ src/user/user-module.ts
@@ -1,20 +1,26 @@
 
+import { inject, injectable } from 'inversify';
+
+// only for types
 import * as Knex from 'knex';
 
-// TODO: inject this
-const knex = Knex({
-  client: 'postgresql',
-  connection: {
-    host     : 'localhost',
-    user     : 'gitlab',
-    password : 'password',
-    database : 'gitlabhq_production',
-    port: '5432',
-  },
-});
+@injectable()
+export default class UserModule {
 
-export async function getPrivateAuthenticationToken(userId: number): Promise<string> {
-  const row = await knex.select('authentication_token')
-    .from('users').where('id', userId).first();
-  return row.authentication_token;
+  public static injectSymbol = Symbol('user-module');
+
+  private gitlabKnex: Knex;
+
+  constructor(@inject('gitlab-knex') gitlabKnex: Knex) {
+    this.gitlabKnex = gitlabKnex;
+  }
+
+  public async getPrivateAuthenticationToken(userId: number): Promise<string> {
+    const row = await this.gitlabKnex.select('authentication_token')
+      .from('users').where('id', userId).first();
+    return row.authentication_token;
+  }
+
 }
+
+

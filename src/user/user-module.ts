41f9cfe88ca40c19e73480f97941f087dbce4181
Juho Ojala <juho@lucify.com>
2016-07-26 17:57:01 +0300

    Remove code from user module that is now in authentication module

--- src/user/user-module.ts
+++ src/user/user-module.ts
@@ -1,37 +1,13 @@
 
-import { inject, injectable } from 'inversify';
-
-// only for types
-import * as Knex from 'knex';
-
+import { injectable } from 'inversify';
 
 @injectable()
 export default class UserModule {
 
   public static injectSymbol = Symbol('user-module');
 
-  private gitlabKnex: Knex;
-  private gitlabBaseUrl: string;
-  private internalServerUrl: string;
+  constructor() {
 
-  constructor(
-    @inject('gitlab-knex') gitlabKnex: Knex,
-    @inject('gitlab-base-url') gitlabBaseUrl: string,
-    @inject('internal-server-url') internalServerUrl: string) {
-    this.gitlabKnex = gitlabKnex;
-    this.gitlabBaseUrl = gitlabBaseUrl;
-    this.internalServerUrl = internalServerUrl;
-  }
-
-  public async getPrivateAuthenticationToken(userId: number): Promise<string> {
-    const row = await this.gitlabKnex.select('authentication_token')
-      .from('users').where('id', userId).first();
-    return row.authentication_token;
-  }
-
-  public async getRootAuthenticationToken() {
-    // TODO: cache this
-    return await this.getPrivateAuthenticationToken(1);
   }
 
 }

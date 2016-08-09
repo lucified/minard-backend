5201343ca7e364029995e55b719db67fe590a25a
Juho Ojala <juho@lucify.com>
2016-08-09 17:41:10 +0300

    Add inversify bindings for activity-module in app.ts

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,7 @@ import { default as SystemHookModule, systemHookBaseUrlSymbol } from './system-h
 
 import HelloPlugin from './hello/hello-hapi-plugin';
 
+import ActivityModule from './activity/activity-module';
 import UserModule from './user/user-module';
 
 import { LocalEventBus, injectSymbol as eventBusInjectSymbol } from './event-bus';
@@ -56,6 +57,7 @@ kernel.bind(ProjectModule.injectSymbol).to(ProjectModule).inSingletonScope();
 kernel.bind(ProjectPlugin.injectSymbol).to(ProjectPlugin);
 kernel.bind(SystemHookModule.injectSymbol).to(SystemHookModule);
 kernel.bind(AuthenticationModule.injectSymbol).to(AuthenticationModule);
+kernel.bind(ActivityModule.injectSymbol).to(ActivityModule);
 
 kernel.bind(JsonApiHapiPlugin.injectSymbol).to(JsonApiHapiPlugin);
 kernel.bind(JsonApiModule.injectSymbol).to(JsonApiModule);

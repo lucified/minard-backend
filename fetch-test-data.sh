6de3b31333ca27d7274ecb3964c118d009ef0863
Ville Väänänen <ville.vaananen@lucify.com>
2016-08-11 16:55:57 +0300

    Only touch existing gitlab-data if downloading succeeds

--- fetch-test-data.sh
+++ fetch-test-data.sh
@@ -5,11 +5,11 @@ set -e
 DIRECTORY="gitlab-data"
 OLD_DIRECTORY="gitlab-data-backup"
 FILENAME="test-data.tar.gz"
+aws s3 cp s3://lucify-configuration/minard/$FILENAME .
 if [ -d $DIRECTORY ]; then
   echo "Backing up existing $DIRECTORY to $OLD_DIRECTORY"
   mv $DIRECTORY $OLD_DIRECTORY
 fi
-aws s3 cp s3://lucify-configuration/minard/$FILENAME .
 tar -xvzf $FILENAME
 rm $FILENAME
 

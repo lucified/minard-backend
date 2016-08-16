dd59a4ebaf44aa21c543d46f115184894b88848c
Juho Ojala <juho@lucify.com>
2016-08-16 21:22:47 +0300

    Add some tests in json-api-module and add pending tests

--- src/json-api/json-api-module-spec.ts
+++ src/json-api/json-api-module-spec.ts
@@ -4,19 +4,97 @@ import 'reflect-metadata';
 import { expect } from 'chai';
 
 import {
+  MinardDeployment,
+} from '../deployment';
+
+import {
   MinardBranch,
+  MinardCommit,
   MinardProject,
+  ProjectModule,
 } from '../project/';
 
 import {
+  ApiBranch,
+  ApiCommit,
+  ApiDeployment,
   ApiProject,
   JsonApiModule,
 } from './';
 
 describe('json-api-module', () => {
 
+  describe('toApiCommit', () => {
+    it('should work when no deployments are passed');
+    it('should work when deployments are are passed');
+  });
+
+  describe('toApiBranch', () => {
+    it('should work when deployments and commits are passed');
+    it('should work when no deployments or commits are passed');
+  });
+
+  describe('toApiActivity', () => {
+    it('should work with activity of type deployment');
+  });
+
+  describe('toApiDeployment', () => {
+    it('should work when commit is not passed', async () => {
+      // Arrange
+      const projectId = 5;
+      const minardDeployment = {
+        id: 2,
+        commitRef: { id: 'foo' },
+        ref: 'master',
+        status: 'success',
+      } as MinardDeployment;
+
+      // Avoid instantiating full projectModule
+      // as we only need one method
+      const projectModule = {} as ProjectModule;
+      projectModule.toMinardCommit = ProjectModule.prototype.toMinardCommit;
+
+      const jsonApiModule = new JsonApiModule({} as any, projectModule, {} as any);
+      jsonApiModule.toApiCommit = async function(
+        projectId: number,
+        commit: MinardCommit,
+        deployments?: ApiDeployment[]) {
+        expect(deployments).to.not.exist;
+        expect(commit.id).to.equal(minardDeployment.commitRef.id);
+        return {
+          id: 'foo-commit',
+        };
+      };
+      const deployment = await jsonApiModule.toApiDeployment(projectId, minardDeployment);
+      expect(deployment).to.exist;
+      expect(deployment.commit.id).to.equal('foo-commit');
+      expect(deployment.id).to.equal('5-2');
+    });
+
+    it('should work when commit is passed', async () => {
+      // Arrange
+      const projectId = 5;
+      const minardDeployment = {
+        id: 2,
+        commitRef: { id: 'foo' },
+        ref: 'master',
+        status: 'success',
+      } as MinardDeployment;
+
+      const apiCommit = {
+        id: 'foo-commit',
+      } as ApiCommit;
+
+      const jsonApiModule = new JsonApiModule({} as any, {} as any, {} as any);
+      const deployment = await jsonApiModule.toApiDeployment(projectId, minardDeployment, apiCommit);
+      expect(deployment).to.exist;
+      expect(deployment.commit.id).to.equal('foo-commit');
+      expect(deployment.id).to.equal('5-2');
+    });
+  });
+
   describe('toApiProject()', () => {
-    it('should work in typical case', async () => {
+    it('should work when no branches are passed', async () => {
       // Arrange
       // -------
       const minardProject = {
@@ -45,8 +123,37 @@ describe('json-api-module', () => {
       // Assert
       // ------
       expect(project.id).to.equal('1');
-      // Make sure branches are converted to APIBranch:es
       expect(project.branches[0].id).to.equal('1-master');
+      expect(project.branches[0].deployments).to.have.length(2);
+    });
+
+    it('should work when branches is passed', async () => {
+      // Arrange
+      // -------
+      const minardProject = {
+        id: 1,
+        branches: [
+          {
+            name: 'master',
+          } as MinardBranch,
+        ],
+      } as MinardProject;
+      const branch = {
+        id: '1-master',
+        deployments: [{}, {}],
+      } as ApiBranch;
+
+      const api = {} as JsonApiModule;
+      api.toApiProject = JsonApiModule.prototype.toApiProject.bind(api);
+
+      // Act
+      // ---
+      const project = await api.toApiProject(minardProject, [branch]);
+
+      // Assert
+      // ------
+      expect(project.id).to.equal(String(minardProject.id));
+      expect(project.branches[0].id).to.equal(branch.id);
       expect(project.branches[0].deployments).to.have.length(2);
     });
   });

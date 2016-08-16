964df2763c9bc79004f35a8749f2ebd26f6563ab
Juho Ojala <juho@lucify.com>
2016-08-16 20:25:17 +0300

    Add tests for activity route in json-api-hapi-plugin

--- src/json-api/json-api-hapi-plugin-spec.ts
+++ src/json-api/json-api-hapi-plugin-spec.ts
@@ -3,18 +3,85 @@ import { expect } from 'chai';
 import 'reflect-metadata';
 import Hapi = require('hapi');
 
-import { get } from '../config';
 import { JsonApiHapiPlugin, parseActivityFilter } from './json-api-hapi-plugin';
 
-const provisionServer = async () => {
-  const plugin = get<JsonApiHapiPlugin>(JsonApiHapiPlugin.injectSymbol);
+const hapiAsyncHandler = require('hapi-async-handler');
+
+const provisionServer = async (plugin: JsonApiHapiPlugin) => {
   const server = new Hapi.Server();
   server.connection({ port: 8080 });
+  await server.register([hapiAsyncHandler]);
   await server.register([plugin]);
   return server;
 };
 
 describe('json-api-hapi-plugin', () => {
+
+  describe('/activity', () => {
+    it('should correctly get project activity', async() => {
+      // Arrange
+      const mockFactory = () => ({
+        getProjectActivity: async function(projectId: number) {
+          return [
+            {
+              id: 'foo',
+            },
+            {
+              id: 'bar',
+            }
+          ]
+        },
+      });
+      const plugin = new JsonApiHapiPlugin(mockFactory as any);
+      const server = await provisionServer(plugin);
+
+      // Act
+      const options: Hapi.IServerInjectOptions = {
+        method: 'GET',
+        url: 'http://foo.com/activity?filter=project[2]',
+      };
+      const ret = await server.inject(options);
+
+      // Assert
+      expect(ret).to.exist;
+      const parsed = JSON.parse(ret.payload);
+      expect(ret.statusCode).to.equal(200);
+      expect(parsed.data).to.have.length(2);
+      expect(parsed.data[0].type).to.equal('activities');
+    });
+
+    it('should correctly get team activity', async() => {
+      // Arrange
+      const mockFactory = () => ({
+        getTeamActivity: async function(projectId: number) {
+          return [
+            {
+              id: 'foo',
+            },
+            {
+              id: 'bar',
+            }
+          ]
+        },
+      });
+      const plugin = new JsonApiHapiPlugin(mockFactory as any);
+      const server = await provisionServer(plugin);
+
+      // Act
+      const options: Hapi.IServerInjectOptions = {
+        method: 'GET',
+        url: 'http://foo.com/activity',
+      };
+      const ret = await server.inject(options);
+
+      // Assert
+      expect(ret).to.exist;
+      const parsed = JSON.parse(ret.payload);
+      expect(ret.statusCode).to.equal(200);
+      expect(parsed.data).to.have.length(2);
+      expect(parsed.data[0].type).to.equal('activities');
+    });
+  });
 
   describe('parseActivityFilter', () => {
     it('should correctly parse filter with projectId', () => {

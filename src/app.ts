a7963f31aa58cf379b1a400b23f56e288f8ab6ba
Juho Ojala <juho@lucify.com>
2016-08-17 21:34:29 +0300

    Run operations-module on startup

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,17 @@
 require('isomorphic-fetch');
 import 'reflect-metadata';
 
+import { OperationsModule } from './operations';
 import { MinardServer } from './server';
 
 import { get } from './config';
 
 const server = get<MinardServer>(MinardServer.injectSymbol);
-
 server.start().catch((err) => {
   server.logger.error('Error starting charles');
+});
+
+const operations = get<OperationsModule>(OperationsModule.injectSymbol);
+operations.runBasicMaintenceTasks().catch(err => {
+  server.logger.error('Error running maintenance tasks', err);
 });

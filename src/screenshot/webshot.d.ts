56919eb89e77b46edc69d7538c99f85cde8bda8b
Juho Ojala <juho@lucify.com>
2016-08-18 10:51:32 +0300

    Fix linter warnings

--- src/screenshot/webshot.d.ts
+++ src/screenshot/webshot.d.ts
@@ -1,6 +1,6 @@
 
-declare module "webshot" {
-  function webshot(url: string, path: string, optiona: any, callback: (err: any) => void) : void;
-  namespace webshot {}
+declare module 'webshot' {
+  function webshot(url: string, path: string, options: any, callback: (err: any) => void): void;
+  namespace webshot {} // tslint:disable-line
   export = webshot;
 }

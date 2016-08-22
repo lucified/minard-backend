4aebd66287ad897854b22c55d98f34f2721fbf3a
Juho Ojala <juho@lucify.com>
2016-08-22 10:04:56 +0300

    Do not run maintenance tasks on startup

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,12 @@
 require('isomorphic-fetch');
 import 'reflect-metadata';
 
-import { OperationsModule } from './operations';
 import { MinardServer } from './server';
 
 import { get } from './config';
 
 const server = get<MinardServer>(MinardServer.injectSymbol);
 server.start().catch((err) => {
-  server.logger.error('Error starting charles');
+  server.logger.error('Error starting charles', err);
 });
 
-const operations = get<OperationsModule>(OperationsModule.injectSymbol);
-operations.runBasicMaintenceTasks().catch(err => {
-  server.logger.error('Error running maintenance tasks', err);
-});

1653dcba96bbb63ffbcef48da274f34c554e82d2
Juho Ojala <juho@lucify.com>
2016-08-23 20:34:41 +0300

    Remove local screenshotter

--- src/config/config-local-override.example.ts
+++ src/config/config-local-override.example.ts
@@ -1,18 +1,64 @@
 
+
 import { Kernel, interfaces } from 'inversify';
+import * as winston from 'winston';
 
+import { goodOptionsInjectSymbol } from '../server';
+import Logger, { loggerInjectSymbol } from '../shared/logger';
 import { ENV } from '../shared/types';
+import { FilterStream } from './utils';
 
-import {
-  screenshotterInjectSymbol,
-} from '../screenshot';
+function requestFilter(data: any) {
+  // ignore successfull requests
+  // if (data.statusCode === 200) {
+  //   return false;
+  // }
+  if (data.path
+      && data.path.indexOf('/ci/api/v1/builds/register.json') !== -1
+      && data.statusCode === 404) {
+    return false;
+  }
+  return true;
+};
 
-// This requires the webshot package to be installed
-import {
-  LocalScreenshotter,
-} from '../screenshot/screenshotter-local';
+const goodOptions = {
+  reporters: {
+    console: [
+      new FilterStream(requestFilter),
+      {
+        module: 'good-squeeze',
+        name: 'Squeeze',
+        args: [
+          {
+            log: '*',
+            response: '*',
+            error: '*',
+          },
+        ],
+      },
+      {
+        module: 'good-console',
+      },
+      'stdout',
+    ],
+  },
+};
+
+const winstonOptions = {
+  transports: [
+    new winston.transports.Console({
+      level: 'info',
+      colorize: true,
+      timestamp: true,
+      prettyPrint: true,
+      silent: false,
+    }),
+  ],
+};
 
 export default (kernel: interfaces.Kernel, env: ENV) => {
-  kernel.unbind(screenshotterInjectSymbol);
-  kernel.bind(screenshotterInjectSymbol).to(LocalScreenshotter);
+  kernel.unbind(goodOptionsInjectSymbol);
+  kernel.bind(goodOptionsInjectSymbol).toConstantValue(goodOptions);
+  kernel.unbind(loggerInjectSymbol);
+  kernel.bind(loggerInjectSymbol).toConstantValue(Logger(winstonOptions));
 };

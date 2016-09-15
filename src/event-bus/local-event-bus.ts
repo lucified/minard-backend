dd562b3b4c7f2e6c7b04ab7b19de7030a29e0b54
Ville Väänänen <ville.vaananen@lucify.com>
2016-09-15 17:01:20 +0300

    Add exception handling to LocalEventBus

--- src/event-bus/local-event-bus.ts
+++ src/event-bus/local-event-bus.ts
@@ -7,18 +7,36 @@ import { EventBus } from './';
 @injectable()
 export default class LocalEventBus implements EventBus {
 
-  protected readonly stream: Subject<Event<any>>;
+  protected readonly stream: Observable<Event<any>>;
+  protected readonly subject: Subject<Event<any>>;
 
   constructor() {
-    this.stream = new Subject<Event<any>>();
+    this.subject = new Subject<Event<any>>();
+    this.stream = this.handledSubject();
+  }
+
+  private handledSubject(): Observable<Event<any>> {
+    return this.subject.catch(err => this.handleError(err));
+  }
+  private handleError(err?: any): Observable<Event<any>> {
+    if (err) {
+      console.error(err);
+    }
+    return this.handledSubject();
   }
 
   public async post(event: Event<any>) {
-    this.stream.next(event);
+    if (this.subject.isStopped) {
+      throw new Error('eventBus has stopped running, which should never happen.');
+    }
+    this.subject.next(event);
     return true;
   }
 
   public getStream(): Observable<Event<any>> {
+    if (this.subject.isStopped) {
+      throw new Error('eventBus has stopped running, which should never happen.');
+    }
     return this.stream;
   }
 
@@ -27,7 +45,7 @@ export default class LocalEventBus implements EventBus {
    * given by the arguments. When giving multiple arguments, the type T should be a discriminated union type.
    */
   public filterEvents<T>(...types: string[]): Observable<Event<T>> {
-    return this.stream.filter(e => types.indexOf(e.type) >= 0)
+    return this.getStream().filter(e => types.indexOf(e.type) >= 0)
       .map(e => e as Event<T>);
   }
 

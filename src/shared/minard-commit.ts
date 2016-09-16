27b1c45196baf2a40aa82f69f90b150db4fcba38
Juho Ojala <juho@lucify.com>
2016-09-16 09:16:19 +0300

    Add missing file src/shared/minard-commit.ts

---
+++ src/shared/minard-commit.ts
@@ -0,0 +1,34 @@
+
+import { Commit } from './gitlab.d';
+
+export interface MinardCommitAuthor {
+  name: string;
+  email: string;
+  timestamp: string;
+}
+
+export interface MinardCommit {
+  id: string;
+  shortId: string;
+  message: string;
+  author: MinardCommitAuthor;
+  committer: MinardCommitAuthor;
+}
+
+export function toMinardCommit(gitlabCommit: Commit): MinardCommit {
+  return {
+    id: gitlabCommit.id,
+    shortId: gitlabCommit.short_id,
+    message: gitlabCommit.message,
+    author: {
+      email: gitlabCommit.author_email,
+      name: gitlabCommit.author_name,
+      timestamp: gitlabCommit.authored_date || gitlabCommit.created_at,
+    },
+    committer: {
+      email: gitlabCommit.committer_email || gitlabCommit.author_email,
+      name: gitlabCommit.committer_name || gitlabCommit.author_name,
+      timestamp: gitlabCommit.committed_date || gitlabCommit.created_at,
+    },
+  };
+}

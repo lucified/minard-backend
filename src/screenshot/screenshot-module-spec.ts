c83045e8ce9764e0b1488d41e712c959f4d3d3ff
Juho Ojala <juho@lucify.com>
2016-09-20 16:54:16 +0300

    Remove obsolete tests for screenshot-module

--- src/screenshot/screenshot-module-spec.ts
+++ src/screenshot/screenshot-module-spec.ts
@@ -17,75 +17,6 @@ describe('screenshot-module', () => {
   const projectId = 4;
   const deploymentId = 12;
 
-  // describe('subscribeToEvents', () => {
-  //   it('should take screenshot when receiving deployment event with status "extracted"', () => {
-  //     // Arrange
-  //     const bus = new LocalEventBus();
-  //     const screenshotModule = new ScreenshotModule(bus, logger, screenshotUrlPattern, {} as any, '', '');
-  //     let called = false;
-  //     screenshotModule.takeScreenshot = async (_projectId, _deploymentId) => {
-  //       expect(_projectId).to.equal(projectId);
-  //       expect(_deploymentId).to.equal(_deploymentId);
-  //       called = true;
-  //     };
-  //     // Act
-  //     bus.post(createDeploymentEvent({
-  //       id: deploymentId,
-  //       projectId: 4,
-  //       status: 'extracted',
-  //     }));
-
-  //     // Assert
-  //     expect(called).to.equal(true);
-  //   });
-
-  //   it('should not take screenshot on other deployment events', () => {
-  //     // Arrange
-  //     const bus = new LocalEventBus();
-  //     const screenshotModule = new ScreenshotModule(bus, logger, screenshotUrlPattern, {} as any, '', '');
-  //     screenshotModule.takeScreenshot = async (_projectId, _deploymentId) => {
-  //       expect.fail('Should not take screenshot');
-  //     };
-  //     // Act
-  //     bus.post(createDeploymentEvent({
-  //       id: deploymentId,
-  //       projectId: 4,
-  //       status: 'success',
-  //     }));
-  //   });
-
-  //   it('should continue after error taking a screenshot', () => {
-  //     // Arrange
-  //     const failProjectId = 5;
-  //     const bus = new LocalEventBus();
-  //     const screenshotModule = new ScreenshotModule(bus, logger, screenshotUrlPattern, {} as any, '', '');
-  //     let called = false;
-  //     screenshotModule.takeScreenshot = async (_projectId, _deploymentId) => {
-  //       if (_projectId === failProjectId) {
-  //         throw Error('foo');
-  //       }
-  //       expect(_projectId).to.equal(projectId);
-  //       expect(_deploymentId).to.equal(_deploymentId);
-  //       called = true;
-  //     };
-  //     // Act
-  //     bus.post(createDeploymentEvent({
-  //       id: deploymentId,
-  //       projectId: 5,
-  //       status: 'extracted',
-  //     }));
-  //     bus.post(createDeploymentEvent({
-  //       id: deploymentId,
-  //       projectId: 4,
-  //       status: 'extracted',
-  //     }));
-
-  //     // Assert
-  //     expect(called).to.equal(true);
-  //   });
-
-  // });
-
   describe('takeScreenshot', () => {
     const baseUrl = 'http://foobar.com';
     it('should call webshot with correct arguments', async () => {
@@ -110,29 +41,6 @@ describe('screenshot-module', () => {
       expect(url).to.equal(`http://4-12.localhost:8000`);
       expect(publicUrl).to.equal(`${baseUrl}/screenshot/${projectId}/${deploymentId}`);
     });
-    // it('should post event', async () => {
-    //   // Arrange
-    //   const webshot = {
-    //     webshot: (_url: string, _path: string, options?: any) => {
-    //       return Promise.resolve(true);
-    //     },
-    //   } as Screenshotter;
-    //   const bus = new LocalEventBus();
-    //   const screenshotModule = new ScreenshotModule(bus, logger, screenshotUrlPattern, webshot, '', baseUrl);
-
-    //   // Act
-    //   screenshotModule.takeScreenshot(projectId, deploymentId);
-    //   const payload = await bus.filterEvents<ScreenshotEvent>(SCREENSHOT_EVENT_TYPE)
-    //     .map(event => event.payload)
-    //     .take(1)
-    //     .toPromise();
-
-    //   // Assert
-    //   expect(payload).to.exist;
-    //   expect(payload.projectId).to.equal(projectId);
-    //   expect(payload.deploymentId).to.equal(deploymentId);
-    //   expect(payload.url).to.equal(`${baseUrl}/screenshot/${projectId}/${deploymentId}`);
-    // });
   });
 
   describe('deploymentHasScreenshot', () => {

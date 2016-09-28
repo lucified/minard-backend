ffa1a8db3baad3794f9e37792a81f9857fa6cbbb
Ville Väänänen <ville.vaananen@lucify.com>
2016-09-28 14:27:25 +0300

    Add environment to the service identifier

--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,16 @@ import { registerService } from './shared/dns-register';
 const migrations = get<Migrations>(Migrations.injectSymbol);
 const server = get<MinardServer>(MinardServer.injectSymbol);
 
+const serviceName = ['charles'];
+if (process.env.LUCIFY_ENV) {
+  serviceName.push(process.env.LUCIFY_ENV);
+}
+
+
 async function start() {
   try {
     await migrations.prepareDatabase();
-    await Promise.all([server.start(), registerService()]);
+    await Promise.all([server.start(), registerService(serviceName.join('-'))]);
   } catch (err) {
     server.logger.error('Error starting charles', err);
   }

8774f590fc9708b79b0ff953c3e253cff2b811eb
Juho Ojala <juho@lucify.com>
2016-10-13 20:26:56 +0300

    Add script for manually testing flowdock

---
+++ src/notification/flowdock-manual-test.ts
@@ -0,0 +1,37 @@
+
+import * as fs from 'fs';
+import 'reflect-metadata';
+
+import { MinardDeployment } from '../deployment';
+import { IFetch, fetch } from '../shared/fetch';
+import { FlowdockNotify } from './flowdock-notify';
+
+const data = fs.readFileSync('mini-camel.jpg') as Buffer;
+
+const flowdockNotify = new FlowdockNotify(fetch as IFetch);
+
+const flowToken = process.env.FLOWDOCK_FLOW_TOKEN;
+const projectUrl = 'http://www.foo.com';
+const branchUrl = 'http://www.bar.com';
+
+const deployment: MinardDeployment = {
+  id: 9,
+  projectId: 5,
+  status: 'success',
+  ref: 'foo-branch',
+  projectName: 'foo-project-name',
+  url: 'http://foo-deployment-url.com',
+  commit: {
+    id: 'foo-id',
+    shortId: 'foo-id',
+    message: 'foo',
+    committer: {
+      name: 'juhoojala',
+      email: 'juho@lucify.com',
+    },
+  },
+  screenshot: data,
+} as any;
+
+flowdockNotify.notify(deployment, flowToken, projectUrl, branchUrl)
+  .catch(err => console.log(err));

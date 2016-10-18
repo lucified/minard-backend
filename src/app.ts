46a30e91d8cbb93430972f87d124066ae9acdf82
Ville Väänänen <ville.vaananen@lucify.com>
2016-10-18 20:46:37 +0300

    Trap exit signals and add delay

--- src/app.ts
+++ src/app.ts
@@ -2,26 +2,53 @@
 // polyfills
 import 'reflect-metadata';
 
-import Migrations from './migrations';
-import { MinardServer } from './server';
+import { Server } from 'hapi';
 
 import { get } from './config';
+import Migrations from './migrations';
+import { MinardServer } from './server';
+import { Logger } from './shared/logger';
 import { Route53Updater } from './shared/route53-updater';
+import { sleep } from './shared/sleep';
 
 const migrations = get<Migrations>(Migrations.injectSymbol);
 const route53updater = get<Route53Updater>(Route53Updater.injectSymbol);
-const server = get<MinardServer>(MinardServer.injectSymbol);
+const minardServer = get<MinardServer>(MinardServer.injectSymbol);
 const localBaseUrl = process.env.ROUTE53_BASEURL_LOCAL;
 const route53Zone = process.env.ROUTE53_ZONE_LOCAL;
 
 async function start() {
   try {
     await migrations.prepareDatabase();
-    await server.start();
+    const server = await minardServer.start();
+    minardServer.logger.info('Charles listening on %s', server.info.uri);
+    trapSignals(server, minardServer.logger);
     await route53updater.update(localBaseUrl, route53Zone);
   } catch (err) {
-    server.logger.error('Error starting charles', err);
+    minardServer.logger.error('Error starting charles', err);
   }
 }
 
 start();
+
+function trapSignals(server: Server, logger: Logger, exitDelay = 10000) {
+
+  server.ext('onPreStop', async (_server, next) => {
+    logger.debug('Starting exit delay');
+    await sleep(exitDelay);
+    logger.debug('Exit delay finished');
+    return next();
+  });
+
+  function stop(signal: string) {
+    return async () => {
+      logger.info('RECEIVED %s', signal);
+      await server.stop();
+      logger.info('Charles is exiting with code 0');
+      process.exit(0);
+    };
+  }
+
+  ['SIGTERM', 'SIGINT'].forEach(signal => process.on(signal, stop(signal)));
+
+}

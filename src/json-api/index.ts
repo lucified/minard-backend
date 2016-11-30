59e780b24c66c89139e4054eedbca7d84117d51d
Juho Ojala <juho@lucify.com>
2016-11-30 19:00:52 +0200

    Add preview endpoint

--- src/json-api/index.ts
+++ src/json-api/index.ts
@@ -1,6 +1,8 @@
 export { JsonApiModule } from './json-api-module';
 export { MemoizedJsonApiModule, memoizeApi } from './memoized-json-api-module';
 export { JsonApiHapiPlugin } from './json-api-hapi-plugin';
+export { ViewEndpoints } from './view-endpoints';
+
 export * from './types';
 
 export const jsonApiInjectSymbol = Symbol('json-api-module');

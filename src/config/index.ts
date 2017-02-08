c06794a15b1ab73d6ea5c960655c95fe2197e82e
Ville Väänänen <ville.vaananen@lucify.com>
2017-02-08 15:52:40 +0200

    Allow accessing the kernel from unit tests
    Also show the loaded environment on startup

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,7 +8,9 @@ import { default as test } from './config-test';
 
 import { ENV } from '../shared/types';
 
-const kernel = new Container();
+// The kernel is exported only for usage in unit tests.
+// A read-only access is provided by the 'get' function.
+export const kernel = new Container();
 kernel.load(common);
 
 interface Configs {
@@ -29,7 +31,7 @@ if (!config) {
 }
 config(kernel);
 override(kernel, env);
-
+console.log(`Loaded configuration for environment '${env}'`);
 export function get<T>(identifier: symbol | string | interfaces.Newable<T> | interfaces.Abstract<T>) {
   return kernel.get<T>(identifier);
 }

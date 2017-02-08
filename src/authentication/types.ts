4b3af543eb6f7988cc8ba7698ef14c75c4efc5e2
Ville Väänänen <ville.vaananen@lucify.com>
2017-02-08 15:50:59 +0200

    Make email optional

--- src/authentication/types.ts
+++ src/authentication/types.ts
@@ -3,12 +3,13 @@ export const gitlabRootPasswordInjectSymbol = Symbol('gitlab-root-password');
 export const jwtOptionsInjectSymbol = Symbol('token-verify-options');
 
 export interface AccessToken {
+  iss: string;
   sub: string;
   aud: string[];
   azp: string;
   exp: number;
   iat: number;
   scope: string;
-  'https://sub_email': string;
+  'https://sub_email'?: string;
   'https://team_token'?: string;
 }

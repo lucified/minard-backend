279d15fba73191d45f563c4a7d559271c1f195fc
Ville Väänänen <ville.vaananen@lucify.com>
2017-03-22 20:02:30 +0200

    Move the memoization boilerplate into this class, where it belongs

--- src/authentication/cached-authentication-hapi-plugin.ts
+++ src/authentication/cached-authentication-hapi-plugin.ts
@@ -1,3 +1,4 @@
+import * as Boom from 'boom';
 import * as auth from 'hapi-auth-jwt2';
 import { inject, injectable } from 'inversify';
 import * as Knex from 'knex';
@@ -32,14 +33,95 @@ class CachedAuthenticationHapiPlugin extends AuthenticationHapiPlugin {
       adminTeamName,
       fetch,
     );
-    this.userHasAccessToProject = memoizee(
-      this.userHasAccessToProject,
-      { promise: true, primitive: true },
+    this.userHasAccessToProjectAsync = memoizee(
+      this.userHasAccessToProjectAsync,
+      { primitive: true, promise: false, async: true },
     );
-    this.userHasAccessToTeam = memoizee(
-      this.userHasAccessToTeam,
-      { promise: true, primitive: true },
+    this.userHasAccessToTeamAsync = memoizee(
+      this.userHasAccessToTeamAsync,
+      { primitive: true, promise: false, async: true },
     );
+    this.isAdminAsync = memoizee(
+      this.isAdminAsync,
+      { primitive: true, promise: false, async: true },
+    );
+  }
+
+  public userHasAccessToTeam(userName: string, teamId: number) {
+    return new Promise((resolve, _reject) => {
+      this.userHasAccessToTeamAsync(userName, teamId, (error: any, result: boolean) => {
+        if (error) {
+          return resolve(false);
+        }
+        return resolve(result);
+      });
+    });
+  }
+  protected userHasAccessToTeamAsync(
+    userName: string,
+    teamId: number,
+    done: (err: any, result: boolean) => void,
+  ) {
+    return this._userHasAccessToTeam(userName, teamId)
+      .then(
+      result => {
+        if (result) {
+          return done(undefined, result);
+        }
+        return done(Boom.notFound(), false); // Don't cache falses
+      },
+      error => done(error, false),
+    );
+  }
+
+  public userHasAccessToProject(userName: string, projectId: number) {
+    return new Promise((resolve, _reject) => {
+      this.userHasAccessToProjectAsync(userName, projectId, (error: any, result: boolean) => {
+        if (error) {
+          return resolve(false);
+        }
+        return resolve(result);
+      });
+    });
+  }
+
+  protected userHasAccessToProjectAsync(
+    userName: string,
+    projectId: number,
+    done: (err: any, result: boolean) => void,
+  ) {
+    return this._userHasAccessToProject(userName, projectId)
+      .then(
+      result => {
+        if (result) {
+          return done(undefined, result);
+        }
+        return done(Boom.notFound(), false); // Don't cache falses
+      },
+      error => done(error, false),
+    );
+  }
+
+  public isAdmin(userIdOrName: string) {
+    return new Promise((resolve, _reject) => {
+      this.isAdminAsync(userIdOrName, (error: any, result: boolean) => {
+        if (error) {
+          return resolve(false);
+        }
+        return resolve(result);
+      });
+    });
+  }
+
+  protected isAdminAsync(
+    userName: string,
+    done: (err: any, result: boolean) => void,
+  ) {
+    return this._isAdmin(userName)
+      .then(
+        result => done(undefined, result), // Cache falses
+        error => done(error, false),
+      );
   }
 }
 

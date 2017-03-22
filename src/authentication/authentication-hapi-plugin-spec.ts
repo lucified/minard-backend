ee518515c676e03eb1881811871b2ef454ddf85e
Ville Väänänen <ville.vaananen@lucify.com>
2017-03-22 09:58:37 +0200

    Improve how the DI container is used
    Add a ‘bootstrap’ method

--- src/authentication/authentication-hapi-plugin-spec.ts
+++ src/authentication/authentication-hapi-plugin-spec.ts
@@ -1,19 +1,20 @@
 import { expect } from 'chai';
+import * as Knex from 'knex';
 import * as moment from 'moment';
 import 'reflect-metadata';
 
-import { get } from '../config';
+import { bootstrap } from '../config';
 import { getAccessToken, issuer } from '../config/config-test';
 import { getTestServer, Server } from '../server/hapi';
 import { fetchMock } from '../shared/fetch';
-import { adminTeamNameInjectSymbol } from '../shared/types';
+import { adminTeamNameInjectSymbol, charlesKnexInjectSymbol } from '../shared/types';
 import {
   accessTokenCookieSettings,
   default as AuthenticationHapiPlugin,
   generatePassword,
 } from './authentication-hapi-plugin';
 import { generateAndSaveTeamToken, generateTeamToken, TeamToken, teamTokenLength } from './team-token';
-import { getDb, insertTeamToken } from './team-token-spec';
+import { initializeTeamTokenTable, insertTeamToken } from './team-token-spec';
 
 const defaultTeamTokenString = generateTeamToken();
 expect(defaultTeamTokenString.length).to.equal(teamTokenLength);
@@ -29,8 +30,11 @@ const validTeamToken: TeamToken = {
   createdAt: moment.utc(),
 };
 
+const kernel = bootstrap('test');
+const knex = kernel.get<Knex>(charlesKnexInjectSymbol);
+
 async function getServer() {
-  const plugin = get<AuthenticationHapiPlugin>(AuthenticationHapiPlugin.injectSymbol);
+  const plugin = kernel.get<AuthenticationHapiPlugin>(AuthenticationHapiPlugin.injectSymbol);
   const server = await getTestServer(true, plugin);
   return server;
 }
@@ -111,7 +115,7 @@ describe('authentication-hapi-plugin', () => {
     it('should require admin team membership to get other teams\' tokens', async () => {
       // Arrange
       const teamId = 2;
-      const adminTeamName = get<string>(adminTeamNameInjectSymbol);
+      const adminTeamName = kernel.get<string>(adminTeamNameInjectSymbol);
 
       fetchMock.mock(/\/groups\?/, [{
         id: 1,
@@ -133,8 +137,8 @@ describe('authentication-hapi-plugin', () => {
     it('should return the team token with GET and a teamId', async () => {
       // Arrange
       const teamId = 2;
-      const adminTeamName = get<string>(adminTeamNameInjectSymbol);
-      const db = await getDb();
+      const adminTeamName = kernel.get<string>(adminTeamNameInjectSymbol);
+      const db = await initializeTeamTokenTable(knex);
       const token = await generateAndSaveTeamToken(teamId, db);
       fetchMock.mock(/\/groups\?/, [{
         id: 1,
@@ -160,7 +164,7 @@ describe('authentication-hapi-plugin', () => {
       // Arrange
       const teamId = 2;
       const teamName = 'foofoo';
-      const db = await getDb();
+      const db = await initializeTeamTokenTable(knex);
       const token = await generateAndSaveTeamToken(teamId, db);
       fetchMock.mock(/\/groups\?/, [{
         id:  teamId,
@@ -186,7 +190,7 @@ describe('authentication-hapi-plugin', () => {
     it('should return 404 if no token found', async () => {
       // Arrange
       const teamId = 23;
-      const adminTeamName = get<string>(adminTeamNameInjectSymbol);
+      const adminTeamName = kernel.get<string>(adminTeamNameInjectSymbol);
 
       fetchMock.mock(/\/groups\?/, [{
         id: 1,
@@ -213,7 +217,7 @@ describe('authentication-hapi-plugin', () => {
     });
     it('should return a new token with POST', async () => {
       // Arrange
-      const adminTeamName = get<string>(adminTeamNameInjectSymbol);
+      const adminTeamName = kernel.get<string>(adminTeamNameInjectSymbol);
 
       fetchMock.mock(/\/groups\?/, [{
         id: 1,
@@ -269,7 +273,7 @@ describe('authentication-hapi-plugin', () => {
   describe('signup endpoint', () => {
     it('should create a gitlab user and add it to the specified group', async () => {
       // Arrange
-      const db = await getDb();
+      const db = await initializeTeamTokenTable(knex);
       await insertTeamToken(db, validTeamToken);
 
       fetchMock.mock(/\/users/, [{
@@ -299,7 +303,7 @@ describe('authentication-hapi-plugin', () => {
 
       // Arrange
       // clear the db
-      await getDb();
+      await initializeTeamTokenTable(knex);
 
       fetchMock.mock(/\/users/, [{
         id: 1,
@@ -325,7 +329,7 @@ describe('authentication-hapi-plugin', () => {
     });
     it('should fall back to trying to retrieve the email from Auth0', async () => {
       // Arrange
-      const db = await getDb();
+      const db = await initializeTeamTokenTable(knex);
       await insertTeamToken(db, validTeamToken);
 
       const invalidEmail = 'foo@';
@@ -411,4 +415,5 @@ describe('authentication-hapi-plugin', () => {
       expect(settings.path).to.eq('/');
     });
   });
+
 });

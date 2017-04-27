e874392c28a05f4256de641ef811fff62ef403b4
Ville Väänänen <ville.vaananen@lucify.com>
2017-04-27 14:06:13 +0300

    Upgrade to the new screenshotter api

--- src/screenshot/types.ts
+++ src/screenshot/types.ts
@@ -4,6 +4,25 @@ export const screenshotFolderInjectSymbol = Symbol('screenshot-folder');
 export const screenshotUrlPattern = Symbol('screenshot-url-pattern');
 
 export interface Screenshotter {
-  webshot(websiteUrl: string, imageFile: string, webshotOptions?: any): Promise<boolean>;
+  save(url: string, dest: string, options?: PageresOptions): Promise<boolean>;
   ping(): Promise<void>;
+}
+
+export interface PageresOptions {
+  delay?: number;
+  timeout?: number;
+  crop?: boolean;
+  css?: string;
+  script?: string;
+  cookies?: string[];
+  filename?: string; // https://github.com/sindresorhus/pageres#filename
+  incrementalName?: boolean;
+  selector?: string;
+  hide?: string[];
+  username?: string;
+  password?: string;
+  scale?: number; // 1
+  format?: 'png' | 'jpg'; // png
+  userAgent?: string;
+  headers?: object;
 }

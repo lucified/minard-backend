61d77373f123a3a5ebc04176e6428e2202b6c31f
Ville Väänänen <ville.vaananen@lucify.com>
2017-05-04 16:49:53 +0300

    Remove obsolete attributes

--- src/json-api/serialization.ts
+++ src/json-api/serialization.ts
@@ -52,8 +52,6 @@ export const notificationSerialization = {
 export const branchSerialization = (apiBaseUrl: string) => ({
   attributes: [
     'name',
-    'description',
-    'project',
     'commits',
     'project',
     'latestCommit',

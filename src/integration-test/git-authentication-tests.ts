0a1f6d6a2b06edb4c27420c0567c44e0c532abb8
Juho Ojala <juho@lucify.com>
2017-06-15 16:54:21 +0300

    Sligthly cleanup git-authentication-tests

--- src/integration-test/git-authentication-tests.ts
+++ src/integration-test/git-authentication-tests.ts
@@ -3,6 +3,11 @@ import 'reflect-metadata';
 import { GitAuthScheme } from '../authentication/git-auth-scheme';
 import { getConfiguration } from './utils';
 
+/*
+ * While theses test only test one class, they are integration tests,
+ * because they are run against the actual Auth0 backend
+ */
+
 const config = getConfiguration(process.env.NODE_ENV);
 const { domain, audience, clientId, clientSecret } = config.auth0.regular;
 
@@ -41,10 +46,9 @@ describe('git-authentication', () => {
       const invalidSecret = secret + '9';
 
       it('should be able to get the accessToken with correct credentials', async () => {
-        // Arrange
-
         // Act
         const response = await scheme.login(id, secret);
+
         // Assert
         expect(response.accessToken).to.exist;
         const parts = response.accessToken.split('.');
@@ -53,10 +57,9 @@ describe('git-authentication', () => {
       });
 
       it('should be able to get the signing key', async () => {
-        // Arrange
-
         // Act
         const response = await scheme.getSigningKey(scheme.decode(accessToken));
+
         // Assert
         expect(response).to.exist;
         expect(typeof response).to.eq('string');
@@ -64,10 +67,9 @@ describe('git-authentication', () => {
       });
 
       it('should verify correct accessToken', () => {
-        // Arrange
-
         // Act
         const response = scheme.verify(accessToken, signingKey);
+
         // Assert
         expect(response.sub).to.exist;
         expect(response.username).to.exist;
@@ -75,8 +77,6 @@ describe('git-authentication', () => {
       });
 
       it('should throw an error with invalid accessToken', () => {
-        // Arrange
-
         // Act
         try {
           scheme.verify(accessToken + 'x', signingKey);
@@ -88,8 +88,6 @@ describe('git-authentication', () => {
       });
 
       it('should throw an error with invalid signingKey', () => {
-        // Arrange
-
         // Act
         try {
           scheme.verify(accessToken, signingKey + 'x');
@@ -101,8 +99,6 @@ describe('git-authentication', () => {
       });
 
       it('should throw an error with incorrect credentials', async () => {
-        // Arrange
-
         // Act
         try {
           await scheme.login(id, invalidSecret);

d6de55d067dc778be7900ea471ad12127799c07e
Ville Väänänen <ville.vaananen@lucify.com>
2017-06-19 16:22:03 +0300

    Test cloning and pushing between projects owned by different teams

--- src/integration-test/tests-inter-team.ts
+++ src/integration-test/tests-inter-team.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import routes, { codes } from './routes';
 import { CharlesClients, EntityType, UserType } from './types';
+import { runCommand } from './utils';
 
 export default (
   clientsFactory: () => Promise<CharlesClients>,
@@ -35,6 +36,95 @@ export default (
       }
     });
   }
+  const userTypes: (keyof CharlesClients)[] = ['regular', 'open'];
+  for (const userType of userTypes) {
+    describe(`${userType} user`, () => {
+      // tslint:disable-next-line:only-arrow-functions
+      it(`should not be able to clone another team\'s project`, async function() {
+        const accessToken = clients[userType].accessToken;
+        const repoFolder = `src/integration-test/blank-cloned`;
+        await runCommand('rm', '-rf', repoFolder);
+        const repoUrl = clients.admin.getRepoUrlWithCredentials({
+          username: accessToken,
+          password: '',
+        });
+        try {
+          await runCommand('git', 'clone', repoUrl, repoFolder);
+          expect.fail(undefined, undefined, 'message');
+        } catch (error) {
+          expect(error.message).to.not.eq('message');
+          console.log(error);
+        }
+      });
+
+      // tslint:disable-next-line:only-arrow-functions
+      it(`should not be able to push to another team\'s project`, async function() {
+        this.timeout(20000);
+        const accessToken = clients[userType].accessToken;
+        const repoFolder = `src/integration-test/blank-cloned`;
+        await runCommand('rm', '-rf', repoFolder);
+        await runCommand(
+          'git',
+          'clone',
+          clients.admin.getRepoUrlWithCredentials(),
+          repoFolder,
+        );
+        const repoUrl = clients.admin.getRepoUrlWithCredentials({
+          username: accessToken,
+          password: '',
+        });
+        await runCommand(
+          'git',
+          '-C',
+          repoFolder,
+          'remote',
+          'add',
+          'minard',
+          repoUrl,
+        );
+        await runCommand('git', '-C', repoFolder, 'remote', 'rm', 'origin');
+        await runCommand('touch', `${repoFolder}/newFile${Date.now()}`);
+        await runCommand('git', '-C', repoFolder, 'add', '-A');
+        await runCommand('git', '-C', repoFolder, 'commit', '-m', 'message');
+        try {
+          await runCommand('git', '-C', repoFolder, 'push', 'minard', 'master');
+          expect.fail(undefined, undefined, 'message');
+        } catch (error) {
+          expect(error.message).to.not.eq('message');
+          console.log(error);
+        }
+      });
+    });
+  }
+  describe('admin user', () => {
+    for (const userType of userTypes) {
+      // tslint:disable-next-line:only-arrow-functions
+      it(`should be able to clone ${userType} user's project`, async function() {
+        this.timeout(20000);
+        const accessToken = clients.admin.accessToken;
+        const repoFolder = `src/integration-test/blank-cloned-admin`;
+        await runCommand('rm', '-rf', repoFolder);
+        await runCommand(
+          'git',
+          'clone',
+          clients[userType].getRepoUrlWithCredentials({
+            username: accessToken,
+            password: '',
+          }),
+          repoFolder,
+        );
+      });
+      // tslint:disable-next-line:only-arrow-functions
+      it(`should be able to push to ${userType} user's project`, async function() {
+        this.timeout(20000);
+        const repoFolder = `src/integration-test/blank-cloned-admin`;
+        await runCommand('touch', `${repoFolder}/newFile${Date.now()}`);
+        await runCommand('git', '-C', repoFolder, 'add', '-A');
+        await runCommand('git', '-C', repoFolder, 'commit', '-m', 'message');
+        await runCommand('git', '-C', repoFolder, 'push', 'origin', 'master');
+      });
+    }
+  });
 };
 
 function getOwnerClient(
